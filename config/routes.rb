624f3e890c2e96c5a50c97ff40383355354730c0
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-22 19:28:35 +0300

    Added jwt, controllers and logic.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  post 'auth_user' => 'authentication#authenticate_user'
+  get 'home' => 'home#index'
+
+  root to: 'home#index'
 end
